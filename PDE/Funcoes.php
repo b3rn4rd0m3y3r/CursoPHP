02d511f38024589d42aae0c00b75b60f2c93aad0
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2023-05-10 18:35:37 -0300

    Add files via upload

--- PDE/Funcoes.php
+++ PDE/Funcoes.php
@@ -5,6 +5,10 @@
 	function i2utf($tx){
 		return iconv("ISO-8859-1","UTF-8", $tx);
 		}
+	function dtSepar($sdate){
+		list($ano, $mes,$dia) = explode("-", $sdate);
+		return $dia . "/" . $mes . "/" . $ano;
+		}
 	header('Content-type: text/html; charset=ISO-8859-1');
 	$arrTIPOS = ["C"=>"text", "D"=>"date", "I"=>"text", "N"=>"number"];
 	// 1 - Leitura da configuração em JSON
@@ -28,4 +32,5 @@
 	if( isset($_GET["Id"]) ){
 		$IDFOUND = 1;
 		}
+	$BACK_MAIN_SCREEN = "<p><span class=big>&#128072;</span><a class=msgBott href=\"?CodUsr=1\">VOLTAR À TELA INICIAL</a></p>";
 ?>
