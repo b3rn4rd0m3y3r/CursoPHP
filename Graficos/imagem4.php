1088fb64a9fca6b7c13425020b8177d9979c6397
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2024-04-03 21:03:11 -0300

    Add files via upload

--- Graficos/imagem4.php
+++ Graficos/imagem4.php
@@ -1,7 +1,15 @@
 <?php
-$img = imagecreatefromjpeg("img.jpg");
-$size = 24; $x = 5; $y = 30; $angle = 0; $quality = 100;
-$color = imagecolorallocate($img, 0, 255, 0);
-imagettftext($img, $size, $x, $y, $angle, $color, "./Roboto.ttf", "TEXT");
-imagejpeg($img, "SAVE.JPG", $quality);
+	header('Content-type: image/jpeg');
+	// Instancia um Canvas a partir de uma imagem pré-existente
+	$img = imagecreatefromjpeg("img.jpg");
+	// Define um pincel
+	$color = imagecolorallocate($img, 0, 255, 0);
+	// Parâmetros de posicionamento de texto
+	$size = 24; $x = 5; $y = 30; $angle = 0; $quality = 100;
+	// Define uma moldura com texto
+	imagettftext($img, $size, $angle, $x, $y, $color, "./Roboto.ttf", "TEXT");
+	imagejpeg($img, "SAVE.JPG", $quality);
+	imagejpeg($img);
+	// Limpa a memória
+	imagedestroy($img);
 ?>
--- Graficos/imagem5.php
+++ Graficos/imagem5.php
@@ -1,19 +1,18 @@
 <?php
 header('Content-type: image/jpeg');
-
-// Load And Create Image From Source
+// Imagem base
 $our_image = imagecreatefromjpeg('img.jpg');
-
-// Allocate A Color For The Text Enter RGB Value
+// Pincel branco
 $white_color = imagecolorallocate($our_image, 255, 255, 255);
 
-// Set Path to Font File
+// Path to Font File
 $font_path = './Roboto.ttf';
 
-// Set Text to Be Printed On Image
+// Textos a serem acrescentados à imagem 
 $text ="Olamptruvel";
 $text2 = "Hugendubel";
 $text3 = "Zarprudel";
+// Parâmetros
 $size=24;
 $angle=30;
 $left=125;
@@ -21,15 +20,13 @@ $top=200;
 $top2=250;
 $top3=380;
 $quality = 100;
-// Print Text On Image
+// Coloca os textos
 imagettftext($our_image, $size,$angle,$left,$top, $white_color, $font_path, $text);
 imagettftext($our_image, $size,$angle,$left,$top2, $white_color, $font_path, $text2);
 imagettftext($our_image, $size,$angle,$left,$top3, $white_color, $font_path, $text3);
-// Send Image to Browser
+// Image to Browser
 imagejpeg($our_image, "SAVE.JPG", $quality);
 imagejpeg($our_image);
-
 // Clear Memory
 imagedestroy($our_image);
-
 ?>
